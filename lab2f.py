aa4b7753ddaaccee7bb3d66d348c30d0380f819f
Aashis Kharel <akharel2@myseneca.ca>
2025-02-06 17:24:46 -0500

    Added lab2f.py on github

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Aashis Kharel
+# Author ID: akharel2
+# Date Created: 2025/02/06
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print (timer)
+    timer = timer -1
+print ('blast off!')
